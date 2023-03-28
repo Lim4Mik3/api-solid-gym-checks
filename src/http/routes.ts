02ac9d550c7452140d9bbfce4d1eccdc4c6d281a
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2023-03-28 00:49:31 -0300

    Feat: create jwt middleware validator

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify';
 import { authenticate } from './controllers/authenticate';
 import { profile } from './controllers/profiile';
 import { register } from './controllers/register';
+import { VerfiyJwt } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
 
   /**  Authenticate routes  */
-  app.get('/me', profile);
+  app.get('/me', { onRequest: [VerfiyJwt] }, profile);
 }
